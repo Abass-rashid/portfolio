f02579c31c44814873cc1675d510414e0bf9aae5
abass rashid <abazrashid144@gmail.com>
2022-06-17 09:38:13 +0300

    Add form-validation with Javascript

--- main.js
+++ main.js
@@ -198,3 +198,24 @@ closeModalBtn.addEventListener('click', () => {
   modal.style.display = 'none';
   body.style.overflow = 'auto';
 });
+
+// form validation
+const form = document.querySelector('.form-contact');
+const email = document.getElementById('email');
+const error = document.querySelector('small');
+form.addEventListener('submit', (e) => {
+  const message = [];
+
+  if (email.value !== email.value.toLowerCase()) {
+    message.push('Please use lowercase for email address');
+  }
+
+  if (message.length > 0) {
+    e.preventDefault();
+    error.innerHTML = message.join('<br/>');
+    email.style.border = '2px solid #df0000';
+    email.style.borderRadius = '3px';
+    email.style.backgroundColor = 'tomato';
+    error.style.backgroundColor = 'red';
+  }
+});
