51fff3c320a5ad1948e931482e4d730558b63ce4
Tarek <83262890+tarek102@users.noreply.github.com>
2022-06-21 09:46:47 +0200

    Add styles for error message on form validation

--- main.js
+++ main.js
@@ -210,5 +210,12 @@ form.addEventListener('submit', (e) => {
     message.push('Please use lowercase for email address');
   }
 
- 
+  if (message.length > 0) {
+    e.preventDefault();
+    error.innerHTML = message.join('<br/>');
+    email.style.border = '2px solid #df0000';
+    email.style.borderRadius = '3px';
+    email.style.backgroundColor = 'tomato';
+    error.style.backgroundColor = 'red';
+  }
 });
